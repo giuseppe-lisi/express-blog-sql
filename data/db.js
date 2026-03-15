6457190b2e34ffb9d291bd09cbda785eb3d696c7
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-15 18:08:20 +0100

    creato modulo per la connessione al db mysql

---
+++ data/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pizzeria'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+
+    console.log("Connessione al db avvenuta con successo");
+})
+
+module.exports = connection;
