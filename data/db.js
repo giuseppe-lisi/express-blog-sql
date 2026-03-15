246aa0d76de714e7d5f7ad1ac61954d19d22eea8
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-15 18:35:16 +0100

    configurata correttamente connessione al db

--- data/db.js
+++ data/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'pizzeria'
+    password: 'root',
+    database: 'blog_db'
 });
 
 connection.connect((err) => {
